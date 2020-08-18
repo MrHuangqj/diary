6d64180c41a6496aaefc0cb95ed04785073a6ce3
翔君 <1733878114@qq.com>
2020-08-18 20:47:31 +0800

    save

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Content from './components/content/index';
+import './index.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+ReactDOM.render(
+  <Content />
+  ,
+  document.getElementById('root')
+);
